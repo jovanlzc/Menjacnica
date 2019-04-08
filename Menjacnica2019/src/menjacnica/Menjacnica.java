c83574caed809ec94eead245a5ad7c8059f2ba5e
jovanlzc <jovan.lazic.10@gmail.com>
2019-04-09 01:09:06 +0200

    Dodata klasa Menjacnica koja implementira interfejs IMenjacnica

---
+++ Menjacnica2019/src/menjacnica/Menjacnica.java
@@ -0,0 +1,29 @@
+package menjacnica;
+
+import java.util.Date;
+
+import imenjacnica.IMenjacnica;
+import model.Kurs;
+import model.Valuta;
+
+public class Menjacnica implements IMenjacnica{
+
+	@Override
+	public void dodajKurs(Valuta valuta, Kurs kurs, Date datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKurs(Valuta valuta, Date datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs pronadjiKurs(Valuta valuta, Date datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
