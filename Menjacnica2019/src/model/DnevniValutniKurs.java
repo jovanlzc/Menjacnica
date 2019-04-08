de4892177fef2d317f71129107f93976cbd65020
jovanlzc <jovan.lazic.10@gmail.com>
2019-04-09 00:51:15 +0200

    Dodate metode equals,hashCode i toString u svakoj od klasa modela

--- Menjacnica2019/src/model/DnevniValutniKurs.java
+++ Menjacnica2019/src/model/DnevniValutniKurs.java
@@ -33,6 +33,45 @@ public class DnevniValutniKurs {
 	public void setDatum(Date datum) {
 		this.datum = datum;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((kurs == null) ? 0 : kurs.hashCode());
+		result = prime * result + ((valuta == null) ? 0 : valuta.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DnevniValutniKurs other = (DnevniValutniKurs) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (kurs == null) {
+			if (other.kurs != null)
+				return false;
+		} else if (!kurs.equals(other.kurs))
+			return false;
+		if (valuta == null) {
+			if (other.valuta != null)
+				return false;
+		} else if (!valuta.equals(other.valuta))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "DnevniValutniKurs [valuta=" + valuta + ", kurs=" + kurs + ", datum=" + datum + "]";
+	}
 	
 	
 }
