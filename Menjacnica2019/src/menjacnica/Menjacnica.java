a8d384faf05b6cf26ab5cfeed81ce4e3879e4a9a
jovanlzc <jovan.lazic.10@gmail.com>
2019-04-09 02:36:27 +0200

    Dodata implementacija metode dodajKurs

--- Menjacnica2019/src/menjacnica/Menjacnica.java
+++ Menjacnica2019/src/menjacnica/Menjacnica.java
@@ -1,16 +1,30 @@
 package menjacnica;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import imenjacnica.IMenjacnica;
+import model.DnevniValutniKurs;
 import model.Kurs;
 import model.Valuta;
 
 public class Menjacnica implements IMenjacnica{
-
+	List<DnevniValutniKurs> dnevniValutniKursevi;
+	
+	public Menjacnica() {
+		dnevniValutniKursevi= new LinkedList<>();
+	}
 	@Override
 	public void dodajKurs(Valuta valuta, Kurs kurs, Date datum) {
 		// TODO Auto-generated method stub
+		if(valuta!=null && kurs!=null && datum!=null) {
+			DnevniValutniKurs dvk = new DnevniValutniKurs(valuta, kurs, datum);
+			dnevniValutniKursevi.add(dvk);
+			return;
+		}
+		throw new RuntimeException("Morate uneti valutu,kurs i datum!");
+		
 		
 	}
 
